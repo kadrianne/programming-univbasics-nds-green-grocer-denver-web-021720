1e52ed645f9a54afa688bb3135b0efdfc590bec4
Kristine Du <kristine.a.du@gmail.com>
2020-02-15 20:52:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -130,7 +130,7 @@ def apply_coupons(cart, coupons)
     cart_item = find_item_by_name_in_collection(item_name, cart)
     coupon_item = find_item_by_name_in_collection(coupon_name, cart)
     if coupon_item == nil
-      coupon = find_item_by_name_in_collection(coupon_name, coupons)
+      coupon = find_item_by_name_in_collection(item_name, coupons)
       coupon_item[:item] = coupon_name
       coupon_item[:price] = coupon[:cost] / coupon[:num]
       coupon_item[:clearance] = cart_item[:clearance]
