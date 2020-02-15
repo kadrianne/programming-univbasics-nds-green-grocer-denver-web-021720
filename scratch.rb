845f37c1fa1d1fa70a38b3bbbe21fb7f34d77800
Kristine Du <kristine.a.du@gmail.com>
2020-02-15 22:16:05 +0000

    Automatically backed up by Learn

--- scratch.rb
+++ scratch.rb
@@ -15,3 +15,65 @@ collection =    [
         cart = [beer, beer, beer]
         beer_coupon = coupons[1]
         coupons = [beer_coupon, beer_coupon]
+        
+def find_item_by_name_in_collection(name, collection)
+  i = 0
+  while i < collection.length do
+    if collection[i][:item] == name
+      return collection[i]
+    else
+      nil
+    end
+    i += 1
+  end
+end
+
+def consolidate_cart(cart)
+  i = 0
+  new_cart = []
+  while i < cart.length do
+    item_name = cart[i][:item]
+    item_new_cart = find_item_by_name_in_collection(item_name, new_cart)
+    if item_new_cart  == nil # if item name is not found in new_cart, add item with :count => 1
+      cart_item = find_item_by_name_in_collection(item_name, cart)
+      new_item_info = {
+        item: cart_item[:item],
+        price: cart_item[:price],
+        clearance: cart_item[:clearance]
+      }
+      new_item_info[:count] = 1
+      new_cart << new_item_info
+    else # if item name is found in new_cart, increase count by 1
+      new_item_info[:count] += 1
+    end
+    i+=1
+  end
+  new_cart
+end
+
+
+def apply_coupons(cart, coupons)
+  i = 0
+  while i < coupons.length
+    item_name = coupons[i][:item]
+    coupon_name = "#{item_name} W/COUPON"
+    cart_item = find_item_by_name_in_collection(item_name, cart)
+    coupon_item = find_item_by_name_in_collection(coupon_name, cart)
+    if coupon_item && coupons[i][:num] >= cart_item[:count]
+      coupon_item[:count] += coupons[i][:num]
+      cart_item[:count] -= coupons[i][:num]
+    elsif !coupon_item
+      coupon_item = {
+      item: coupon_name,
+      price: (coupons[i][:cost] / coupons[i][:num]),
+      clearance: cart_item[:clearance],
+      count: coupons[i][:num]
+      }
+      cart << coupon_item
+      cart_item[:count] -= coupons[i][:num]
+    else
+    end
+    i += 1
+  end
+  cart
+end
