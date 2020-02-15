1cde20abdec90028b7ea87fa04e593bbbca14a02
Kristine Du <kristine.a.du@gmail.com>
2020-02-15 21:14:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -158,7 +158,7 @@ def apply_clearance(cart)
   i = 0
   while i < cart.length do
     if cart[i][:clearance] == true
-      cart[i][:price] = (cart[i][:price]*(.8))
+      cart[i][:price] = (cart[i][:price]*0.8)
     end
   i += 1
   end
